098351c177b2ccc98bb2bb0b3034e1cfa22eb6c9
RobertWardenga <Robert.Wardenga@gmail.com>
2022-01-24 10:00:18 +0100

    fixed return type of reset

--- gym_maze/envs/maze_env.py
+++ gym_maze/envs/maze_env.py
@@ -71,7 +71,7 @@ class MazeEnv(gym.Env):
         return [seed]
 
     def step(self, action):
-        if action in [0,1,2,3]:
+        if action in [0, 1, 2, 3]:
             self.maze_view.move_robot(self.ACTION[action])
         else:
             self.maze_view.move_robot(action)
